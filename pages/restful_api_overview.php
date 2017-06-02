be7953c047fe18579df332970a7e4715f23dfe92
MyCodebox <myCodebox@users.noreply.github.com>
2017-06-03 00:18:31 +0200

    Work on jwt

--- pages/restful_api_overview.php
+++ pages/restful_api_overview.php
@@ -1,28 +1,79 @@
 <?php
-
 	$url = rex_url::currentBackendPage([
 		'rex-api-call'=>'restfull_api'
 	], false);
+?>
 
-	echo <<<EOD
-		<pre class="result"></pre>
-		<script type="text/javascript">
-			$.post( '$url', function( data ) {
-				var json = JSON.stringify(data);
-				$( ".result" ).html( json );
-			});
+<pre class="data"></pre>
+<pre class="jwt"></pre>
+<pre class="claim"></pre>
 
-			// $.post( '$url', function( data ) {
-			// 	var json = JSON.stringify(data);
-			// 	var decode = decodeToken(data);
-			// 	$( ".result" ).html( decode );
-			//
-			// });
-			//
-			// function decodeToken(jwt){
-		    //     var a = jwt.split(".");
-		    //     return  b64utos(a[1]);
-		    // }
 
-		</script>
-EOD;
+<script type="text/javascript">
+	$(function(){
+		var store = store || {};
+
+		store.decodeToken = function(jwt){
+			var a = jwt.split(".");
+			return  b64utos(a[1]);
+		}
+
+		store.setJwt = function(data){
+			this.jwt = data;
+			this.claim = this.decodeToken(data);
+		}
+
+		$.post('<?php echo $url; ?>', function(data){
+			store.setJwt(data.jwt);
+
+			var data_str = JSON.stringify(data);
+			$( ".data" ).html( data_str );
+		});
+	});
+</script>
+
+<!-- <script type="text/javascript">
+	$.post('<?php echo $url; ?>', function(data){
+
+		var data = JSON.stringify(data);
+		$( ".data" ).html( data );
+
+		console.log(data.jwt);
+
+        // var jwt = ecodeToken(data.jwt);
+		// $( ".jwt" ).html( jwt );
+
+		// // var claim 	= this.decodeToken(jwt);
+		//
+		// $( ".claim" ).html( claim );
+    }).fail(function(){
+        alert('error');
+	});
+
+	function ecodeToken(jwt){
+		var a = jwt.split(".");
+		return  b64utos(a[1]);
+	}
+</script> -->
+
+<!--
+<script type="text/javascript">
+	$.post( '$url', function( data ) {
+		var json = JSON.stringify(data);
+		$( ".result" ).html( json );
+	});
+
+	// $.post( '$url', function( data ) {
+	// 	var json = JSON.stringify(data);
+	// 	var decode = decodeToken(data);
+	// 	$( ".result" ).html( decode );
+	//
+	// });
+	//
+	// function decodeToken(jwt){
+    //     var a = jwt.split(".");
+    //     return  b64utos(a[1]);
+    // }
+
+</script>
+ -->
