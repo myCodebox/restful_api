4fc8b047566c0b3d1036dce788fa115c0f2a2160
MyCodebox <myCodebox@users.noreply.github.com>
2017-06-03 00:32:02 +0200

    Add some js and decode the jwt

--- pages/restful_api_overview.php
+++ pages/restful_api_overview.php
@@ -4,9 +4,10 @@
 	], false);
 ?>
 
-<pre class="data"></pre>
+<pre class="req"></pre>
 <pre class="jwt"></pre>
 <pre class="claim"></pre>
+<pre class="data"></pre>
 
 
 <script type="text/javascript">
@@ -23,57 +24,17 @@
 			this.claim = this.decodeToken(data);
 		}
 
-		$.post('<?php echo $url; ?>', function(data){
-			store.setJwt(data.jwt);
+		$.post('<?php echo $url; ?>', function(req){
+			store.setJwt(req.jwt);
 
-			var data_str = JSON.stringify(data);
-			$( ".data" ).html( data_str );
+			var req_str = JSON.stringify(req);
+			$('.req').html( req_str );
+			$('.jwt').html( store.jwt );
+			$('.claim').html( store.claim );
+
+			var parsedJSON = JSON.parse(store.claim);
+			var parsedJSON_str = JSON.stringify(parsedJSON.data);
+			$('.data').html( parsedJSON_str );
 		});
 	});
 </script>
-
-<!-- <script type="text/javascript">
-	$.post('<?php echo $url; ?>', function(data){
-
-		var data = JSON.stringify(data);
-		$( ".data" ).html( data );
-
-		console.log(data.jwt);
-
-        // var jwt = ecodeToken(data.jwt);
-		// $( ".jwt" ).html( jwt );
-
-		// // var claim 	= this.decodeToken(jwt);
-		//
-		// $( ".claim" ).html( claim );
-    }).fail(function(){
-        alert('error');
-	});
-
-	function ecodeToken(jwt){
-		var a = jwt.split(".");
-		return  b64utos(a[1]);
-	}
-</script> -->
-
-<!--
-<script type="text/javascript">
-	$.post( '$url', function( data ) {
-		var json = JSON.stringify(data);
-		$( ".result" ).html( json );
-	});
-
-	// $.post( '$url', function( data ) {
-	// 	var json = JSON.stringify(data);
-	// 	var decode = decodeToken(data);
-	// 	$( ".result" ).html( decode );
-	//
-	// });
-	//
-	// function decodeToken(jwt){
-    //     var a = jwt.split(".");
-    //     return  b64utos(a[1]);
-    // }
-
-</script>
- -->
